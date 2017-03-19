17cbcc5a9bc754adb7d245412d197ae3dd17d2b9
Ilya Bogdanov <fumlead@gmail.com>
2017-03-19 23:35:59 +0300

    Basic tcod example

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,57 @@
+extern crate tcod;
+use tcod::console::{Root, Console, FontLayout, FontType, BackgroundFlag};
+use tcod::colors;
+use tcod::input::Key;
+use tcod::input::KeyCode::{Up, Down, Left, Right, Escape, Enter};
+
+const SCREEN_WIDTH: i32 = 80;
+const SCREEN_HEIGHT: i32 = 50;
+
+const LIMIT_FPS: i32 = 20;
+
+fn handle_input(root: &mut Root, player_x: &mut i32, player_y: &mut i32) -> bool {
+    let keypress = root.wait_for_keypress(true);
+    match keypress {
+        Key { code: Enter, alt: true, .. } => {
+            let fullscreen = !root.is_fullscreen();
+            root.set_fullscreen(fullscreen);
+        }
+        Key { code: Escape, .. } => {
+            return true
+        }
+        Key { code: Up, .. } => *player_y -= 1,
+        Key { code: Down, .. } => *player_y += 1,
+        Key { code: Left, .. } => *player_x -= 1,
+        Key { code: Right, .. } => *player_x += 1,
+        _ => {}
+    }
+    return false;
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut root = Root::initializer()
+    .font("arial10x10.png", FontLayout::Tcod)
+    .font_type(FontType::Greyscale)
+    .size(SCREEN_WIDTH, SCREEN_HEIGHT)
+    .title("Gladiator")
+    .init();
+
+    tcod::system::set_fps(LIMIT_FPS);
+
+    let mut player_x = SCREEN_WIDTH / 2;
+    let mut player_y = SCREEN_HEIGHT / 2;
+
+    while !root.window_closed() {
+        root.set_default_foreground(colors::WHITE);
+        root.put_char(player_x, player_y, '@', BackgroundFlag::None);
+
+        root.flush();
+
+        root.put_char(player_x, player_y, ' ', BackgroundFlag::None);
+
+        let exit = handle_input(&mut root, &mut player_x, &mut player_y);
+        if exit {
+            break
+        }
+    }
 }
